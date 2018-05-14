ca4b91e4adfba91670ac55a06d1d6bd336c2ac42
irismanriquezc <imanriquez@csumb.edu>
2018-05-14 13:35:48 -0700

    added comments to notes files along with the header

--- main.py
+++ main.py
@@ -211,7 +211,8 @@ pointY = 0
 while(1):
     ret, videoImg = cap.read()
     videoImg = cv2.flip(videoImg, 1)
-
+''' gray gets the makes the video into a grayscale form in order for the fist to be recognized easier
+    fist detects the actual fist from the gray variable. '''
     gray = cv2.cvtColor(videoImg, cv2.COLOR_BGR2GRAY)
 
     fist = fist_cascade.detectMultiScale(gray, 1.3,5)
--- notes.py
+++ notes.py
@@ -1,3 +1,18 @@
+'''
+COURSE: CST 205 Multimedia Design and Programing
+TITLE:  FreeHand
+AUTHORS:
+            Iris Manriquez | Daniel Morales | Edgar Reyes
+DATE:   May 13th, 2018
+ABSTRACT:
+
+This notes file creates the individual notes for the piano to access. The notes are saved to the computer in wav files,
+there are different frequencies for each note which gives it a unique sound.
+
+'''
+
+
+
 from scipy.io.wavfile import write
 from pylab import plot,show,axis
 from numpy import linspace,sin,pi,int16
@@ -8,7 +23,7 @@ def note(freq, len, amp=1, rate=44100):
     data = sin(2*pi*freq*t)*amp
     return data.astype(int16) # two byte integers
 
-# A tone, 2 seconds, 44100 samples per second
+# A tone, 1 second, 44100 samples per second
 toneA = note(440,1,amp=10000)
 toneBF= note(466,1,amp=10000)
 toneB = note(494,1,amp=10000)
@@ -24,7 +39,7 @@ toneAF= note(831,1,amp=10000)
 toneA2= note(880,1,amp=10000)
 
 write('Anote.wav',44100,toneA)
-write('BFnote.wav',44100,toneBF)  # writing the sound to a file
+write('BFnote.wav',44100,toneBF)  # writing the sound to files
 write('Bnote.wav',44100,toneB)
 write('Cnote.wav',44100,toneC)
 write('CSnote.wav',44100,toneCS)
